e976d141117189b4d77888c36e7ece1eb77b3fa5
unknown <nathan.sanchez@epita.fr>
2023-06-19 13:23:14 +0200

    tf

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var slideIndex = 1;
+loadFooter();
+loadHeader();
 showSlides(slideIndex);
-
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
@@ -25,4 +26,25 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
+}
+
+function loadFooter() {
+    fetch('../templates/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insérer le contenu du fichier dans la div avec l'id "header"
+            console.log(data);
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.log(error));
+}
+
+function loadHeader() {
+    fetch('../templates/header.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insérer le contenu du fichier dans la div avec l'id "header"
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.log(error));
 }
