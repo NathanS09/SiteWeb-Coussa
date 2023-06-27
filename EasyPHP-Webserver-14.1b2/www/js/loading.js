87bdeaeb1197ceafa8fd6190fb54c3222cabc000
NathanS09 <nathansanchez@sfr.fr>
2023-06-27 21:34:23 +0200

    fix header + teams

--- EasyPHP-Webserver-14.1b2/www/js/loading.js
+++ EasyPHP-Webserver-14.1b2/www/js/loading.js
@@ -1,6 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://unpkg.com/leaflet/dist/leaflet.js';
-document.head.appendChild(script);
 
 loadFooter();
 loadHeader();
@@ -9,7 +6,6 @@ function loadFooter() {
         .then(response => response.text())
         .then(data => {
             // Insérer le contenu du fichier dans la div avec l'id "header"
-            console.log(data);
             document.getElementById('footer').innerHTML = data;
         })
         .catch(error => console.log(error));
@@ -25,14 +21,3 @@ function loadHeader() {
         .catch(error => console.log(error));
 }
 
-function loadFooterMap() {
-    var map = L.map('map').setView([43.04407, 1.69493], 13);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-        maxZoom: 18,
-        subdomains: 'abc' // Utilisation de sous-domaines par défaut
-    }).addTo(map);
-
-    L.marker([43.04407, 1.69493]).addTo(map);
-}
