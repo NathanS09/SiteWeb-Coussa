b4d61aec39279887d625089895b205f87f773e4f
unknown <nathan.sanchez@epita.fr>
2023-06-30 19:34:28 +0200

    tt

---
+++ www/WebApp/WebApp/FormController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
+
+namespace WebApp { 
+
+    public class ContactData
+{
+        public string Nom { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+
+
+    public class ContactController : Controller
+    {
+        private readonly IEmailService _emailService;
+
+        public ContactController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        [Route("/FormController/Submit")]
+        public async Task<IActionResult> Submit(string nom, string email, string message)
+        {
+            // Traitement des données du formulaire...
+
+            // Envoi de l'email
+            await _emailService.SendEmailAsync("bontahzno@gmail.com", nom, message);
+
+            // Redirection vers une page de confirmation
+            return RedirectToAction("Confirmation");
+        }
+
+        public IActionResult Confirmation()
+        {
+            // Afficher une page de confirmation
+            return View();
+        }
+    }
+
+}
---
+++ www/WebApp/WebApp/Services/EmailService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using WebApp.Services;
+
+namespace WebApp.Services
+{
+
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string SmtpUsername { get; set; }
+        public string SmtpPassword { get; set; }
+        public bool EnableSsl { get; set; }
+        public string FromEmail { get; set; }
+    }
+
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string nom, string body)
+        {
+            string smtpHost = _configuration["EmailSettings:SmtpHost"];
+            int smtpPort = Convert.ToInt32(_configuration["EmailSettings:SmtpPort"]);
+            string smtpUsername = _configuration["EmailSettings:SmtpUsername"];
+            string smtpPassword = _configuration["EmailSettings:SmtpPassword"];
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(smtpUsername);
+            mailMessage.To.Add(toEmail);
+            mailMessage.Subject = "Nouveau message de contact";
+            mailMessage.Body = body;
+            mailMessage.IsBodyHtml = true;
+
+            using (SmtpClient smtpClient = new SmtpClient(smtpHost, smtpPort))
+            {
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                smtpClient.EnableSsl = true;
+
+                await smtpClient.SendMailAsync(mailMessage);
+            }
+        }
+    }
+
+}
